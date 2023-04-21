2d1b71964339b8ea7606ae7b06da2e4fdefed73a
Adithya Singh <41729963+adithyagaurav@users.noreply.github.com>
2023-04-21 12:54:39 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 #  MIT License
 #   
-#  Copyright (c) 2022 Adithya Singh, Rishabh Singh, Divyansh Agrawal
+#  Copyright (c) 2022 Adithya Singh
 #   
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
